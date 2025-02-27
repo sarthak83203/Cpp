8b384df74bb3e757f4b6a278e436ca189aa99c9a
sarthak <sarthak.shinde2006@gmail.com>
2025-02-27 14:10:01 +0530

    Floyd's Triangle

---
+++ floydstri.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+class Matrix{
+    public:
+    int n;
+    Matrix(int size){
+        n=size;
+    }
+    void display(){
+        int num=1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                cout<<num<<" ";
+                num++;
+  
+            }
+            
+            cout<<endl;
+        }
+    }
+    ~Matrix(){
+        cout<<"Destructor is called";
+    }
+};
+int main(){
+    int p;
+    cin>>p;
+    Matrix s1(p);
+    s1.display();
+    return 0;
+}
