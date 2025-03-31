90fa83bf87aebf92566d00e77698702dddf8bae0
sarthak <sarthak.shinde2006@gmail.com>
2025-03-31 10:53:23 +0530

    Virtual

---
+++ virtual2.cpp
@@ -0,0 +1,76 @@
+/*
+Single File Programming Question
+Problem Statement
+
+
+
+Sharon is working on a population growth analysis program. She is studying how populations change over time. 
+
+She wants to write a program that calculates the growth rate of a population based on its initial and final sizes.
+
+Help Sharon write a program that includes a base class Popul and a derived class Birth. Popul has a virtual function calcRate() to initialize the initial and final sizes. Class Birth inherits from Popul, overriding calcRate() to calculate the growth rate based on births.
+
+
+
+Note: Growth Rate = (1000 * (final polulation - initial population) / initial population)
+
+Input format :
+The first line of input consists of an integer N, representing the initial population.
+
+The second line consists of an integer P, representing the final population.
+
+Output format :
+The output prints a double value, representing the population growth rate, rounded off to two decimal places.
+
+
+
+Refer to the sample output for formatting specifications.
+
+Code constraints :
+In this scenario, the test cases fall under the following constraints:
+
+10 ≤ N ≤ 107
+
+10 ≤ P ≤ 107
+
+Sample test cases :
+Input 1 :
+100
+1000
+Output 1 :
+9000.00
+Input 2 :
+456
+768
+Output 2 :
+684.21
+*/
+#include<iostream>
+#include<iomanip>
+using namespace std;
+class Popul{
+    public:
+    double final,initial;
+    virtual void calcRate(){
+        cin>>initial>>final;
+    }
+    
+    
+};
+class Birth:public Popul{
+    public:
+    void calcRate() {
+        Popul::calcRate();
+        double pi=(1000 * (final - initial)/initial);
+        cout<<fixed<<setprecision(2)<<pi;
+    }
+    
+    
+};
+int main(){
+    Popul s1;
+    Birth s2;
+    
+    s2.calcRate();
+    return 0;
+}
